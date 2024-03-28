7ae3fd720f1de9c8cd7df824ceac14b375688165
Viktor Gustafsson <viktor.gustafsson@bannerflow.com>
2024-03-28 16:33:54 +0100

    Day 5

---
+++ Day5/LazyMap.cs
@@ -0,0 +1,33 @@
+﻿namespace Day5;
+
+public class LazyMap
+{
+    private readonly List<(long sourceStart, long range, long offset)> _mappings = [];
+
+    public void AddRangeMapping(string mapping)
+    {
+        var parts = mapping.Split(" ");
+        var destinationStart = long.Parse(parts[0]);
+        var sourceStart = long.Parse(parts[1]);
+        var range = long.Parse(parts[2]);
+        var offset = destinationStart - sourceStart;
+
+        _mappings.Add((sourceStart, range, offset));
+    }
+
+    public long? this[long? source]
+    {
+        get
+        {
+            foreach (var (sourceStart, range, offset) in _mappings)
+            {
+                if (source >= sourceStart && source < sourceStart + range)
+                {
+                    return source + offset;
+                }
+            }
+
+            return source;
+        }
+    }
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,68 @@
+﻿using Day5;
+
+var data = File.ReadAllText("C:\\develop\\AdventOfCode2023\\Day5\\input.txt");
+var strings = data.Split("\r\n\r\n");
+var seeds = strings[0].Split(':')[1].Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+var seedToSoilMapInput = GetMap(strings[1]);
+var soilToFertilizerMapInput = GetMap(strings[2]);
+var fertilizerToWaterMapInput = GetMap(strings[3]);
+var waterToLightMapInput = GetMap(strings[4]);
+var lightToTemperatureMapInput = GetMap(strings[5]);
+var temperatureToHumidityMapInput = GetMap(strings[6]);
+var humidityToLocationMapInput = GetMap(strings[7]);
+
+var seedToSoilMap = ConstructMap(seedToSoilMapInput);
+var soilToFertilizerMap = ConstructMap(soilToFertilizerMapInput);
+var fertilizerToWaterMap = ConstructMap(fertilizerToWaterMapInput);
+var waterToLightMap = ConstructMap(waterToLightMapInput);
+var lightToTemperatureMap = ConstructMap(lightToTemperatureMapInput);
+var temperatureToHumidityMap = ConstructMap(temperatureToHumidityMapInput);
+var humidityToLocationMap = ConstructMap(humidityToLocationMapInput);
+
+long? result = null;
+foreach (var seed in seeds)
+{
+    var seedLocation = FindSeedLocation(long.Parse(seed));
+    if (result is null || result > seedLocation)
+    {
+        result = seedLocation;
+    }
+}
+
+long? FindSeedLocation(long? seedId)
+{
+    //seed to soil
+    var soilId = seedToSoilMap[seedId];
+    // soil to fertilizer
+    var fertilizerId = soilToFertilizerMap[soilId];
+    // fertilizer to water
+    var waterId = fertilizerToWaterMap[fertilizerId];
+    // water to light
+    var lightId = waterToLightMap[waterId];
+    // light to temperature
+    var temperatureId = lightToTemperatureMap[lightId];
+    //temperature to humidity
+    var humidityId = temperatureToHumidityMap[temperatureId];
+    // humidity to location
+    var locationId = humidityToLocationMap[humidityId];
+    
+    return locationId;
+}
+
+LazyMap ConstructMap(string[] mapInput)
+{
+    var map = new LazyMap();
+    foreach (var input in mapInput)
+    {
+        map.AddRangeMapping(input);
+    }
+
+    return map;
+}
+
+string[] GetMap(string mapAsString)
+{
+    return mapAsString.Split("\r\n")[1..];
+}
+
+Console.WriteLine(result);
