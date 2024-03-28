ed995fddbfbdd7564d616ad996a4dcd0d56c7371
Viktor Gustafsson <viktor.gustafsson@bannerflow.com>
2024-03-28 17:23:20 +0100

    Day6 Part 2

---
+++ Day6_part2/Program.cs
@@ -0,0 +1,25 @@
+﻿using Day6_part2;
+
+var races = new List<Race>
+{
+    new()
+    {
+        Time = 56977875,
+        DistanceToBeat = 546192711311139
+    }
+};
+
+foreach (var race in races)
+{
+    for (long speed = 0; speed < race.Time; speed++)
+    {
+        var distance = (race.Time - speed) * speed;
+
+        if (distance > race.DistanceToBeat)
+        {
+            race.WaysToWin++;
+        }
+    }
+}
+
+Console.WriteLine(races.Select(x=>x.WaysToWin).Aggregate((total, next) => total * next));
---
+++ Day6_part2/Race.cs
@@ -0,0 +1,8 @@
+﻿namespace Day6_part2;
+
+public class Race
+{
+    public int Time { get; set; }
+    public long DistanceToBeat { get; set; }
+    public int WaysToWin { get; set; }
+}
